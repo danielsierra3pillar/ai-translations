d082e5836d6e29facfbdf2f2d4144587cd85a905
Daniel Sierra <daniel.sierra@3pillarglobal.com>
2022-10-18 16:50:15 -0600

    Sql script for translations api

---
+++ script.sql
@@ -0,0 +1,115 @@
+
+
+create database translations;
+
+
+use translations;
+
+drop table translation;
+CREATE TABLE translation(
+	idTranslation INTEGER PRIMARY KEY auto_increment,
+	english varchar(200),
+    spanish varchar(200),
+    native varchar(200),
+    lenguage varchar(100)
+);
+
+
+select * from translation;
+
+
+  insert into translation (english, spanish, native, lenguage)
+  values("person", "persona", "Achi", "quiche") ; 
+  insert into translation (english, spanish, native, lenguage)
+  values("bicycle", "bicicleta", "B'siklet", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("car", "carro", "Chi'ich' reb'al jastaq", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("motorcycle", "motocicleta", "Mot", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("airplane", "avion", "Ch'ich' kab'in pa kaj", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("bus", "bus", "Chi'ich' ereb'al winaq", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("train", "tren", "Yuquyik ch'ich'", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("boat", "bote", "Nim jukub'", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("stop sign", "señal de transito", "K’ulb’a’t", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("bench", "silla, trono, banca", "Tem", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("bird", "pájaro", "tzʼikin", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("cat", "gato", "Me’s", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("dog", "perro", "Tz’i’", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("horse", "caballo", "Kej", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("sheep", "oveja", "Karne'l", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("cow", "vaca", "Wakax", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("backpack", "bolso", "umoral", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("handbag", "mochila", "umoral", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("cup", "taza", "Xaru’", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("fork", "tenedor", "Ixk’yaq", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("knife", "cuchillo", "Q’atb’al || Pilb’al", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("spoon", "cuchara", "Pak'", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("bowl", "recipiente", "Tol", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("banana", "banano", "Ankane'y", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("apple", "manzana", "Mansa'n", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("orange", "naranja", "Alanxax", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("chair", "silla", "Tem", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("couch", "sillon", "Nimatem", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("bed", "cama", "Ch'at", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("dining table", "mesa de centro", "Mexa", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("toilet", "sanitario", "B'alb'al chuluj", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("tv", "television", "ka'yib'al wachib'al", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("laptop", "computadora", "Kematz'ib' ", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("mouse", "raton", "Ch'o", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("cell phone", "telefono", "Ch'ab'eb'al", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("book", "libro", "Wuj", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("scissors", "tijeras", "Tuxe'r", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("telephone", "telefono", "Ch'ab'eb'al", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("house", "casa", "Achoch", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("tree", "arbol", "che'", "quiche");
+  insert into translation (english, spanish, native, lenguage)
+  values("person", "persona", "Winäq", "kaqchikel");
+  insert into translation (english, spanish, native, lenguage)
+  values("book", "libro", "sik´iwuj", "kaqchikel");
+
+
+
+
+
+
+
+
+
+
+
